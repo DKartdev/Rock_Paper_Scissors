fb15688a6a39a1977688ff9754469b097e87bfba
Darta Kesane <252036818+DKartdev@users.noreply.github.com.>
2026-05-04 13:30:44 +0300

    add repetition of game 5 times and score counting
    created for loop to repeat gameround 5 times. created function countHumanScore && countComputerScore
    to count score of gameround

--- rock_paper_scissors_.js
+++ rock_paper_scissors_.js
@@ -1,34 +1,31 @@
-function playGame(){ 
-    for(i = 1; i <= 3; i++){//repeats the code 5 times
 
-function getRndInteger() {
-  return Math.floor(Math.random() * (4 - 1) + 1);// returns integer from 1 to 3 both including. 
+function roundRepetiotion(humanChoice, computerChoice){
+    
+    for(i = 1; i <= 5; i++){
+
+function getHumanChoice(){
+
+  let input = prompt("Please enter your choice!");//gets human input rock, paper or scissors
+  let stringToLower = input.toLowerCase()
+  let strLenght = input.length;
+  let firstLetter = stringToLower.slice(0, 1);
+  let firstLetterUper = firstLetter.toUpperCase();
+  let restOfTheWord = stringToLower.slice(1, strLenght);
+  let humanInput = firstLetterUper + restOfTheWord;
+  return humanInput
 }
+
+let humanChoice = getHumanChoice();
+
 
  const paper = "Paper";
  const rock = "Rock";
  const scissors = "Scissors";
 
 
-function getHumanChoice(){
-    let input = prompt("Please enter your choice!");//gets human input rock, paper or scissors
-
-
-  let stringToLower = input.toLowerCase()
-
-  let strLenght = input.length;
-
-  let firstLetter = stringToLower.slice(0, 1);
-  let firstLetterUper = firstLetter.toUpperCase();
-  let restOfTheWord = stringToLower.slice(1, strLenght);
-
-  let humanInput = firstLetterUper + restOfTheWord;
-
-
-
-    return humanInput
+function getRndInteger() {
+  return Math.floor(Math.random() * (4 - 1) + 1);// returns integer from 1 to 3 both including. 
 }
-
 
 
 function getComputerChoice(){//gives random rock, paper or scissors, based on random number from fuction getRndInteger.
@@ -49,71 +46,151 @@ function getComputerChoice(){//gives random rock, paper or scissors, based on ra
     return choice;
 }
 
-
-let humanChoice = getHumanChoice();//need to convert to case insensitive
 let computerChoice = getComputerChoice();
-let humanScore = 0
-let computerScore = 0
 
-let selectionInRound = "Human choice:" + " " + humanChoice  + " " + humanScore + " " + "||| " + "Computer choice:" + " " + computerChoice + " " + computerScore;
+
+const human = "human"
+const computer = "computer"
+
+let humanScore = 0; 
+let computerScore = 0;
+
+// let theScore =  playGame()//gets the score for winner. this probably isnt working right becasue it returns 1 but doesnt say to hum, its like score eaquls both humanScore and computerScore
+// console.log(theScore )
+
+
+
+
+let selectionInRound = "Human choice:" + " " + humanChoice  +  " " + "||| " + " " + "Computer choice:" + " " + computerChoice ;
 console.log(selectionInRound)
 
 
+//// This far works well as intended
 
+
+
+
+    ////start of the game 
+
+   
 function playRound(humanChoice, computerChoice){
  //  plays a single round, increments the round winner’s score and logs a winner announcement.
+ 
+  let winner; 
+ 
  if (humanChoice === paper && computerChoice === rock){
-     
-    console.log("Human Wins!")
- }
+    winner = human;
+
+     }
  else if(humanChoice === paper && computerChoice === scissors ){
-   
-    console.log("Computer Wins!")
+   winner = computer
+    
  }
   else if(humanChoice === paper && computerChoice === paper ){
     console.log("it`s a Tie!")
+
  }
   else if(humanChoice === rock && computerChoice === rock ){
     console.log("it`s a Tie!")
  }
  else if(humanChoice === rock && computerChoice === scissors ){
-     
-    console.log("Human Wins!")
+   winner = human
+  
  }
  else if(humanChoice === rock && computerChoice === paper ){
-    
-    console.log("Computer Wins!")
+    winner = computer
+   
  }
  else if(humanChoice === scissors && computerChoice === scissors ){
     console.log("It`s a Tie!")
  }
  else if(humanChoice === scissors && computerChoice === rock ){
-    
-    console.log("Computer Wins!")
- }
+   winner = computer
+   
+   }
  else if(humanChoice === scissors && computerChoice === paper ){
-    
-    console.log("Human Wins!")
+    winner = human
+     
  }
  
  else{console.log("Error! Please try again!")}
-
-}
  
-playRound(humanChoice, computerChoice)
-  
- function contScore(){
-
-
-
- }
-
-
-
-
+  return winner;
         
-        
-}//end of playgame()
+}
+
     
+  
+
+
+
+    
+function roundWinner(){
+
+let winnerOfRound = playRound(humanChoice, computerChoice);
+return winnerOfRound
+
+}//returns who won the round
+
+
+
+
+
+   
+ 
+
+
+
+let winner = roundWinner()
+
+
+
+
+function countHumaScore(){
+
+let addHumanScore;
+if (winner === human){
+    addHumanScore = humanScore++;
+    addHumanScore++
+}
+else {addHumanScore = humanScore} 
+return addHumanScore
 
 }
+  
+let countHumaScoref = countHumaScore()
+console.log("Human score:" + " " + countHumaScoref)
+
+
+
+function countComputerScore(){
+let addComputerScore
+if (winner === computer){
+    addComputerScore = computerScore++;
+  addComputerScore++
+
+
+}
+else {addComputerScore = computerScore} 
+return addComputerScore
+}
+
+let countComputerScoref = countComputerScore()
+console.log("Computer score:" + " " + countComputerScoref)
+
+
+}
+}
+
+  /// theScore + humanScore++ 
+
+//playGame() play the game 
+
+
+
+
+
+
+
+
+roundRepetiotion()
